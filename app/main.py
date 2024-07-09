7a0f05cb71a5fcca0405102dccf7904410504ff5
Oğulcan Günaydın <ogulcangunaydin@outlook.com>
2024-07-09 22:55:57 +0300

    Add cors

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
-import logging
 from dotenv import load_dotenv
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-load_dotenv()  # take environment variables from .env.
-
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi import FastAPI
 from . import models, db_operations, routers
 from .database import engine
 import os
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+load_dotenv()  # take environment variables from .env.
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 
 app.include_router(routers.router)
