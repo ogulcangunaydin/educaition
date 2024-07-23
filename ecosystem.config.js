8b6762b45396cca96d25203601537f0522334b4b
Oğulcan Günaydın <ogulcangunaydin@outlook.com>
2024-07-23 21:05:28 +0300

    fix branch name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       user: 'ec2-user',
       host: 'ec2-54-173-57-250.compute-1.amazonaws.com',
       key: '~/.ssh/id_rsa.pub',
-      ref: 'origin/master',
+      ref: 'origin/main',
       repo: 'git@github.com:ogulcangunaydin/educaition.git',
       path: '/home/ec2-user/educaition/source',
       'post-deploy': 'python3.10 -m venv env && pip install -r requirements.txt && pm2 startOrRestart ecosystem.config.js'
